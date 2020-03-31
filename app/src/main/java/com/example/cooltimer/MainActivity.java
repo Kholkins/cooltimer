f065dc42c7e4370e7cb1f084dc6f9ad5778fd690
Kholkin <kholkins@mail.ru>
2020-03-31 18:16:23 +0300

    new

--- app/src/main/java/com/example/cooltimer/MainActivity.java
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -6,15 +6,55 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CountDownTimer downTimer = new CountDownTimer(10000,1000) {
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(60);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress/60;
+                int seconds = progress - (minutes*60);
+
+                String strMinutes = "";
+                String strSeconds = "";
+
+                if (minutes<10) {
+                    strMinutes = "0"+minutes;
+                }else strMinutes = String.valueOf(minutes);
+
+                if (seconds<10) {
+                    strSeconds = "0"+seconds;
+                }else strSeconds = String.valueOf(seconds);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        /*CountDownTimer downTimer = new CountDownTimer(10000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 Log.d("timer", String.valueOf(millisUntilFinished/1000)+"seconds left.");
@@ -25,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("timer", "finish");
             }
         };
-        downTimer.start();
+        downTimer.start();*/
 
        /* final Handler handler = new Handler();
 
