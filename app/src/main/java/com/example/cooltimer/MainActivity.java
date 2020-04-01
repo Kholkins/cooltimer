565724a5cd46ca6223922dc4d174423d7a10c689
Kholkin <kholkins@mail.ru>
2020-04-01 18:56:54 +0300

    menu

--- app/src/main/java/com/example/cooltimer/MainActivity.java
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cooltimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -9,6 +11,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -135,5 +138,23 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.timer_memu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            Intent openSettings = new Intent(this, SettingsActivity.class);
+            startActivity(openSettings);
+            return true;
+        }
+        if (id == R.id.action_about){
+            Intent openAbout = new Intent(this, AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
