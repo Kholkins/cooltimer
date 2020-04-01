115eaadbfae1379cc5fac26a11029a3af75ab030
Kholkin <kholkins@mail.ru>
2020-04-01 20:38:58 +0300

    menu

--- app/src/main/java/com/example/cooltimer/AboutActivity.java
+++ app/src/main/java/com/example/cooltimer/AboutActivity.java
@@ -1,6 +1,5 @@
 package com.example.cooltimer;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,9 +11,6 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        ActionBar actionBar = this.getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+
     }
 }
