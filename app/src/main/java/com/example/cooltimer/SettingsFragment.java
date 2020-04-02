bda18e7c410d0bc74c5f24fc0b6a2bc8f8c09eb8
Kholkin <kholkins@mail.ru>
2020-04-02 18:43:15 +0300

    add array melody

--- app/src/main/java/com/example/cooltimer/SettingsFragment.java
+++ app/src/main/java/com/example/cooltimer/SettingsFragment.java
@@ -1,13 +1,27 @@
 package com.example.cooltimer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.timer_preferences);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+        }
+    }
+    private void setPreferenceLabel (Preference preference, String value) {
+
     }
 }
