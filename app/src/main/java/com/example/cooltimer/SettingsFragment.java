879c189c202f2f7156ea934dba3dedcda3755673
Kholkin <kholkins@mail.ru>
2020-04-02 11:32:01 +0300

    settings fragment

--- app/src/main/java/com/example/cooltimer/SettingsFragment.java
+++ app/src/main/java/com/example/cooltimer/SettingsFragment.java
@@ -1,5 +1,13 @@
 package com.example.cooltimer;
 
-public class SettingsFragment {
+import android.os.Bundle;
 
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
 }
